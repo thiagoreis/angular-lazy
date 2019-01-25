c30cb7ef95760ebc7d11782276a744ab4e4b68ac
Thiago Reis <thiagoreis128@gmail.com>
2019-01-25 16:41:05 -0200

    Generate About component

--- src/app/lazy/lazy.module.ts
+++ src/app/lazy/lazy.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LazyRoutingModule } from './lazy-routing.module';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [AboutComponent],
   imports: [
     CommonModule,
     LazyRoutingModule
